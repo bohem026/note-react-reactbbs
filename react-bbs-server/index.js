68aac71284bcce4865f5ddc09e5072c5ebf56d17
bohem dev <microbin98@gmail.com>
2026-07-02 14:50:32 +0900

    feat: implement MySQL connection and add /list endpoint; remove unused axios test

--- react-bbs-server/index.js
+++ react-bbs-server/index.js
@@ -1,17 +1,37 @@
 const express = require('express');
 var cors = require('cors');
 const app = express();
+const mysql = require('mysql');
 const port = 3000;
 
 var corsOptions = {
-  origin: '*'
+  origin: '*',
 };
 
 app.use(cors(corsOptions));
 
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'bbs',
+});
+
+db.connect();
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.get('/list', (req, res) => {
+  const sqlQuery = 'SELECT * FROM board;';
+
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+db.end();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
