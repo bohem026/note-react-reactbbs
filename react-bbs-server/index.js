acca1f94acb0597737d19a0e9789a05ea7d79109
bohem dev <microbin98@gmail.com>
2026-07-02 13:41:45 +0900

    Add CORS support and Axios integration; update dependencies
    - Implemented CORS in the server to allow cross-origin requests.
    - Added Axios for making HTTP requests in the BoardList component.
    - Updated package.json and package-lock.json to include new dependencies.
    - Created a .gitignore file to exclude node_modules from version control.

--- react-bbs-server/index.js
+++ react-bbs-server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const app = express()
-const port = 3000
+var cors = require('cors');
+const app = express();
+const port = 3000;
+
+var corsOptions = {
+  origin: '*'
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
