0abde22c1877cbcb3aa0cf50c48af01297088c61
bohem dev <microbin98@gmail.com>
2026-07-06 16:24:10 +0900

    feat: implement file deletion functionality for uploaded images in delete and update endpoints

--- react-bbs-server/index.js
+++ react-bbs-server/index.js
@@ -4,6 +4,8 @@ const app = express();
 const mysql = require('mysql2');
 const port = 3000;
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 app.use(express.json()); // json-> object
 app.use(express.urlencoded({ extended: true })); //html form ->object
@@ -38,9 +40,20 @@ const db = mysql.createConnection({
 
 db.connect();
 
+function deleteUploadedFile(filePath) {
+  if (!filePath) return;
+
+  const absolutePath = path.resolve(filePath); // 삭제할 파일의 절대 경로 확인
+  if (fs.existsSync(absolutePath)) {
+    // 실제 서버 있는지 확인
+    fs.unlinkSync(absolutePath);
+  }
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
 app.get('/list', (req, res) => {
   const sqlQuery = "SELECT id, title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board;";
   db.query(sqlQuery, (err, result) => {
@@ -48,6 +61,7 @@ app.get('/list', (req, res) => {
     res.send(result);
   });
 });
+
 app.get('/view', (req, res) => {
   console.log(req.query.id);
   const id = req.query.id;
@@ -76,6 +90,13 @@ app.post('/write', upload.single('image'), (req, res) => {
 app.post('/delete', (req, res) => {
   console.log(req.body);
   const { id } = req.body;
+
+  // 글 번호 삭제할 이미지의 경로 파악
+  db.query('SELECT image_path FROM board WHERE id=?', [id], (err, result) => {
+    if (err) throw err;
+    const existingImagePath = result[0] ? result[0].image_path : null;
+    deleteUploadedFile(existingImagePath);
+  });
 
   const sqlQuery = 'DELETE FROM board WHERE id=?;';
   db.query(sqlQuery, [id], (err, result) => {
@@ -110,6 +131,8 @@ app.post('/update', upload.single('image'), (req, res) => {
      * 이미지 삭제 요청 O + 새이미지 X
      * ->기존이미지 제거, image_path 값 비우기
      */
+    // 서버에서 기존 이미지 삭제
+
     sqlQuery = 'UPDATE board SET writer=?, title=?, content=?, image_path=NULL WHERE id=?';
     params = [writer, title, content, id];
   } else if (imagePath) {
